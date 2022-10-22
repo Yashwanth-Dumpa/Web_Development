7ea4dc4337708a48fad1e26ba6f1d4ecf61086aa
Yashwanth-Dumpa <109025252+Yashwanth-Dumpa@users.noreply.github.com>
2022-10-22 13:35:39 +0530

    Multiple Todo items

--- Todo List/script.js
+++ Todo List/script.js
@@ -1,33 +1,48 @@
 let todoItemsEle = document.getElementById("todoItemsContainer");
 
-let todoEle = document.createElement("li");
-todoEle.classList.add("d-flex", "flex-row", "todo-item-container");
-todoItemsEle.appendChild(todoEle);
+let todoList = [{
+    text: "Learn HTML",
+    id: "1"
+}, {
+    text: "Learn CSS",
+    id: "2"
+}, {
+    text: "Learn Javascript",
+    id: "3"}];
 
-let inputEle = document.createElement("input");
-inputEle.type = "checkbox";
-inputEle.classList.add("checkbox-input");
-inputEle.id = "checkboxInput";
-todoEle.appendChild(inputEle);
+function createAndAppendTodo(todo) {
+    let todoEle = document.createElement("li");
+    todoEle.classList.add("d-flex", "flex-row", "todo-item-container");
+    todoItemsEle.appendChild(todoEle);
+    
+    let inputEle = document.createElement("input");
+    inputEle.type = "checkbox";
+    inputEle.classList.add("checkbox-input");
+    inputEle.id =todo.id;
+    todoEle.appendChild(inputEle);
+    
+    let labelContainerEle = document.createElement("div");
+    labelContainerEle.classList.add("d-flex", "flex-row", "label-container");
+    todoEle.appendChild(labelContainerEle);
+    
+    
+    let labelEle = document.createElement("label");
+    labelEle.textContent = todo.text;
+    labelEle.setAttribute("for",todo.id);
+    labelEle.classList.add("checkbox-label");
+    labelContainerEle.appendChild(labelEle);
+    
+    let delContEle = document.createElement("div");
+    delContEle.classList.add("delete-icon-container");
+    labelContainerEle.appendChild(delContEle);
+    
+    let iconEle = document.createElement("i");
+    iconEle.classList.add("far","fa-trash-alt","delete-icon");
+    delContEle.appendChild(iconEle);
+}
 
-let labelContainerEle = document.createElement("div");
-labelContainerEle.classList.add("d-flex", "flex-row", "label-container");
-todoEle.appendChild(labelContainerEle);
-
-
-let labelEle = document.createElement("label");
-labelEle.textContent = "Learn HTML";
-labelEle.setAttribute("for","checkboxInput");
-labelEle.classList.add("checkbox-label");
-labelContainerEle.appendChild(labelEle);
-labelContainerEle.appendChild();
-
-let delContEle = document.createElement("div");
-delContEle.classList.add("delete-icon-container");
-labelContainerEle.appendChild(delContEle);
-
-let iconEle = document.createElement("i");
-iconEle.classList.add("far","fa-trash-alt","delete-icon");
-delContEle.appendChild(iconEle);
+for (let item of todoList){
+    createAndAppendTodo(item);
+}
 
 console.log(todoItemsEle);
